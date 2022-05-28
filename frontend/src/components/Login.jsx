28a29f694193334a86e7cdfe903047656b9a9a6d
Thanske <Steven.thans@telenet.be>
2022-05-28 13:30:18 +0200

    Implementation Routes

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,24 +1,36 @@
-import React from 'react'
-
-const Login = () => {
+const Login = props => {
+  const { className } = props;
   return (
     <>
-      <div class="form-popup" id="myForm">
+      <div class={className} id="myForm">
         <form action="" class="form-container">
           <h1>Login</h1>
 
-          <label for="email"><b>Email</b></label>
-          <input type="text" placeholder="Enter Email" name="email" required/>
+          <label for="email">
+            <b>Email</b>
+          </label>
+          <input type="text" placeholder="Enter Email" name="email" required />
 
-          <label for="psw"><b>Password</b></label>
-          <input type="password" placeholder="Enter Password" name="psw" required/>
+          <label for="psw">
+            <b>Password</b>
+          </label>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            name="psw"
+            required
+          />
 
-          <button type="submit" class="btn">Login</button>
-          <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+          <button type="submit" class="btn">
+            Login
+          </button>
+          <button type="button" class="btn cancel" onclick="closeForm()">
+            Close
+          </button>
         </form>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,32 @@
-import Button from "./base_components/Button";
-import Img from "./base_components/Img";
+import Button from "../base_components/Button";
+import Img from "../base_components/Img";
+import { Link } from "react-router-dom";
 
 const Navbar = props => {
   const { className } = props;
   return (
     <>
       <div className={className}>
-        <div className="left">
-          <Img className="img" src="/src/image/Logo.png" alt="Logo" />
-          <Button className="home">Home</Button>
-          <Button className="contact">Contact</Button>
-        </div>
+        <nav className="left">
+          <Img className="img" src="/src/image/Logo.jpg" alt="Logo" />
 
-        <Button className="login_register" onclick="openForm()">Login | Register</Button>
+          <Button className="home_btn">
+            <Link className="link" to="/">
+              Home
+            </Link>
+          </Button>
+          <Button className="contact">
+            <Link className="link" to="/about">
+              About
+            </Link>
+          </Button>
+        </nav>
+
+        <Button className="login_register" onclick="openForm()">
+          <Link className="link" to="/login_register">
+            Login | Register
+          </Link>
+        </Button>
       </div>
     </>
   );
