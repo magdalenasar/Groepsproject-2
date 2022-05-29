4c76f9cd8420e1787b0c70a939b43ae3ba46c1e6
Magdalena Sarandeva <sarandeva2@gmail.com>
2022-05-30 01:42:05 +0200

    Adding placeholders for form

--- frontend/src/components/body_components/About.jsx
+++ frontend/src/components/body_components/About.jsx
@@ -9,10 +9,14 @@ const About = props => {
        <Title>Contact us</Title>
       <div className="contact">
         <div className="contact-form">
-          <form>      
-            <input className="name feedback-input half" type="text" placeholder="Name" />
-            <input className="email feedback-input half" type="text" placeholder="Email" />
-            <textarea className="text feedback-input " placeholder="Message"></textarea>
+          <form>   
+            
+            <label for="name"><b>Name</b></label>
+            <input className="name feedback-input half" type="text" placeholder="Your name here" />
+            <label for="email"><b>Email</b></label>
+            <input className="email feedback-input half" type="text" placeholder="Enter your mail" />
+            <label for="message"><b>Message</b></label>
+            <textarea className="text feedback-input " placeholder="Write your message here..."></textarea>
             <input type="submit" value="Send message"/>
           </form>
         </div>
