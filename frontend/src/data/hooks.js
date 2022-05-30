aac2f38eea54f369034168099b70fd70fe3d41eb
Tijl P <tijlp.business@gmail.com>
2022-05-30 20:02:14 +0200

    update useAxios

---
+++ frontend/src/data/hooks.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export function useAxios(path) {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios(path);
+        setLoading(false);
+        setError(false);
+        setData(data);
+      } catch (error) {
+        setLoading(false);
+        setError(true);
+      }
+    })();
+  }, []);
+  return [data, loading, error];
+}
